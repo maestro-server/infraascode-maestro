f8c28e12722c65ddf5c5c89ad7b0c2ee8e3c77e7
Felipe Signorini <felipeklerk@yahoo.com.br>
2020-04-17 22:33:13 +0100

    update docker entry

--- docker-all-in-one/docker-entrypoint.sh
+++ docker-all-in-one/docker-entrypoint.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Client
-INDEX_PATH="/var/www/index.html"
-VARS=("api_url" "analytics_url" "websocket_url" "static_url" "logo_url" "theme")
+INDEX_PATH=/var/www/index.html
+VARS=('api_url' 'analytics_url' 'websocket_url' 'static_url' 'logo_url' 'theme')
 ENVS=("API_URL" "ANALYTICS_URL" "WEBSOCKET_URL" "STATIC_URL" "LOGO" "THEME")
 
 for i in "${!VARS[@]}"; do
  
     # Reset all vars
-    sed -i "" "s#<meta name='${VARS[$i]}' content='[[:alnum:]\/:\.\ _]*'>#<!-- ${VARS[$i]} -->#" $INDEX_PATH
+    sed -i "s#<meta name='${VARS[$i]}' content='[[:alnum:]\/:\.\ _]*'>#<!-- ${VARS[$i]} -->#" $INDEX_PATH
 
     # If a env vars exit put the value on meta tag
     if [ -n "${!ENVS[$i]}" ];
     then
-        sed -i "" "s#<!-- ${VARS[$i]} -->#<meta name='${VARS[$i]}' content='${!ENVS[$i]}'>#" $INDEX_PATH
+        sed -i "s#<!-- ${VARS[$i]} -->#<meta name='${VARS[$i]}' content='${!ENVS[$i]}'>#" $INDEX_PATH
     fi
 done
 
